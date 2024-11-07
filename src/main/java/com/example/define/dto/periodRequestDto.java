4f49d0b473161cd53dd8fbe91cfb555902d12e38
Resfeber <69678585+Resfebel@users.noreply.github.com>
2024-11-07 14:35:30 +0900

    날짜 별 조회

--- src/main/java/com/example/define/dto/periodRequestDto.java
+++ src/main/java/com/example/define/dto/periodRequestDto.java
@@ -1,10 +1,10 @@
 package com.example.define.dto;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 public class periodRequestDto {
     private int user_code;
     private String start;
